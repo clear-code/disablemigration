40651a370ea13b0f306322eb1a12ff41ac28b407
SHIMODA Hiroshi <shimoda@clear-code.com>
2009-05-12 06:05:39 +0000

    Linuxでビルドが成功するように修正

--- disablemigration.sh
+++ disablemigration.sh
@@ -3,19 +3,9 @@
 appname=${0##*/}
 appname=${appname%.sh}
 
-rm -r jar_temp
+chmod -cfr 644 *.jar *.js *.light *.inf *.rdf *.cfg *.manifest
 rm $appname.jar
 
-mkdir jar_temp
-cp -r content ./jar_temp/
-cp -r locale ./jar_temp/
-cp -r skin ./jar_temp/
+zip -r0 "$appname.jar" content locale skin -x \*/.svn/\*
 
-cd jar_temp
-chmod -cfr 644 *.jar *.js *.light *.inf *.rdf *.cfg *.manifest
-
-zip -r0 "../$appname.jar" content locale skin -x \*/.svn/\*
-cd ..
-
-rm -r jar_temp
 
