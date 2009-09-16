cbb710bbc320d14762091c93ac41f5ec0f5ce8f3
SHIMODA Hiroshi <shimoda@clear-code.com>
2009-09-16 06:01:58 +0000

    自動インポート対応

---
+++ disablemigration.js
@@ -0,0 +1,6 @@
+pref("extensions.disablemigration.autoMigration.enabled", true);
+pref("extensions.disablemigration.autoMigration.migrateAsManual", true);
+pref("extensions.disablemigration.autoMigration.target", "ie"); // ie, opera, dogbert, safari, seamonkey
+// see nsIBrowserProfileMigrator
+// 32 = bookmarks
+pref("extensions.disablemigration.autoMigration.items", 32);
